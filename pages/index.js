7d0a02a8acff6106675ee66296e8312f50d3b870
Jia Yuan <jia.yuan@stablecorp.ca>
2022-11-24 17:42:33 -0500

    purchase and redeem

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,106 @@
 import axios from "axios";
 import { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container, Form, Tab, Tabs } from "react-bootstrap";
 
 export default function Index() {
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [toCurrency, setToCurrency] = useState("CAD");
-  const [rate, setRate] = useState("")
+  const [redeemRate, setRedeemRate] = useState("");
+  const [purchaseRate, setPurchaseRate] = useState("");
+  const [key, setKey] = useState("redeem");
 
   const currencies = ["USD", "CAD"]
 
   async function getFx(from, to) {
     const response = await axios.get(`https://api.coinbase.com/v2/exchange-rates?currency=${from}`);
     const currRate = response.data.data.rates[to];
-    setRate(currRate);
+    if (key === "redeem") {
+      setRedeemRate(currRate);
+    } else {
+      setPurchaseRate(currRate);
+    }
   }
 
   return (
-    <Container className="h-screen flex items-center justify-center">
-      <div className="bg-neutral-200 p-4 rounded-4 w-[350px]">
-        <Form>
-          <Form.Group className="mb-3">
-            <Form.Label>From Currency</Form.Label>
-            <Form.Select disabled value={fromCurrency} onChange={(e) => setFromCurrency(e.currentTarget.value)}>
-              {currencies.map((item, idx) => (
-                <option key={idx}>{item}</option>
-              ))}
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>To Currency</Form.Label>
-            <Form.Select disabled value={toCurrency} onChange={(e) => setToCurrency(e.currentTarget.value)}>
-              {currencies.map((item, idx) => (
-                <option key={idx}>{item}</option>
-              ))}
-            </Form.Select>
-          </Form.Group>
-          {rate && <div className="flex justify-between"><span>Raw FX rate:</span><span ><b>{parseFloat(rate).toFixed(5)}</b></span></div>}
-          {rate && <div className="flex justify-between"><span>Purchase rate + 40bp:</span><span ><b>{(parseFloat(rate) * (1 + 0.004)).toFixed(5)}</b></span></div>}
-          {rate && <div className="flex justify-between"><span>Redeem rate - 40bp:</span><span ><b>{(parseFloat(rate) * (1 - 0.004)).toFixed(5)}</b></span></div>}
-          <br />
-          <Button onClick={() => getFx(fromCurrency, toCurrency)}>
-            Get Rate
-          </Button>
-        </Form>
-      </div>
+    <Container className="h-screen flex flex-column items-center justify-center">
+      <Tabs
+        defaultActiveKey="redeem"
+        activeKey={key}
+        onSelect={k => setKey(k)}
+      >
+        <Tab eventKey="redeem" title="Redeem">
+          <div className="bg-neutral-200 p-4 rounded-4 w-[350px]">
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>From Currency</Form.Label>
+                {/* <Form.Select disabled value={fromCurrency} onChange={(e) => setFromCurrency(e.currentTarget.value)}>
+                  {currencies.map((item, idx) => (
+                    <option key={idx}>{item}</option>
+                  ))}
+                </Form.Select> */}
+                <Form.Control 
+                  disabled
+                  defaultValue="USD"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>To Currency</Form.Label>
+                {/* <Form.Select disabled value={toCurrency} onChange={(e) => setToCurrency(e.currentTarget.value)}>
+                  {currencies.map((item, idx) => (
+                    <option key={idx}>{item}</option>
+                  ))}
+                </Form.Select> */}
+                <Form.Control 
+                  disabled
+                  defaultValue="CAD"
+                />
+              </Form.Group>
+              {redeemRate && <div className="flex justify-between"><span>Raw FX rate:</span><span ><b>{parseFloat(redeemRate).toFixed(5)}</b></span></div>}
+              {redeemRate && <div className="flex justify-between"><span>Redeem rate - 40bp:</span><span ><b>{(parseFloat(redeemRate) * (1 - 0.004)).toFixed(5)}</b></span></div>}
+              <br />
+              <Button onClick={() => getFx("USD", "CAD")}>
+                Get Rate
+              </Button>
+            </Form>
+          </div>
+        </Tab>
+        <Tab eventKey="purchase" title="Purchase">
+          <div className="bg-neutral-200 p-4 rounded-4 w-[350px]">
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>From Currency</Form.Label>
+                {/* <Form.Select disabled value={fromCurrency} onChange={(e) => setFromCurrency(e.currentTarget.value)}>
+                  {currencies.map((item, idx) => (
+                    <option key={idx}>{item}</option>
+                  ))}
+                </Form.Select> */}
+                <Form.Control 
+                  disabled
+                  defaultValue="CAD"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>To Currency</Form.Label>
+                {/* <Form.Select disabled value={toCurrency} onChange={(e) => setToCurrency(e.currentTarget.value)}>
+                  {currencies.map((item, idx) => (
+                    <option key={idx}>{item}</option>
+                  ))}
+                </Form.Select> */}
+                <Form.Control 
+                  disabled
+                  defaultValue="USD"
+                />
+              </Form.Group>
+              {purchaseRate && <div className="flex justify-between"><span>Raw FX rate:</span><span ><b>{parseFloat(purchaseRate).toFixed(5)}</b></span></div>}
+              {purchaseRate && <div className="flex justify-between"><span>Purchase rate + 40bp:</span><span ><b>{(parseFloat(purchaseRate) * (1 + 0.004)).toFixed(5)}</b></span></div>}
+              <br />
+              <Button onClick={() => getFx("CAD", "USD")}>
+                Get Rate
+              </Button>
+            </Form>
+          </div>
+        </Tab>
+      </Tabs>
     </Container>
   )
 }
